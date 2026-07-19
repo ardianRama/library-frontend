113dd448617dd7d8f6fa6d829c74223ed8b68558
Ardian Rama <ardian.rama@hotmail.se>
2026-07-19 15:07:50 +0200

    add protected routes for authenticated and admin-only pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 import HomePage from './pages/HomePage'
 import { LoginPage, RegisterPage } from './pages/AuthPages'
 import BooksPage from './pages/BooksPage'
-import Footer from './components/Footer'
 import LoansPage from './pages/LoansPage'
 import AdminPage from './pages/AdminPage'
+import ProtectedRoute from './components/ProtectedRoute'
 
 export default function App() {
   return (
@@ -17,9 +18,21 @@ export default function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
-          <Route path="/books" element={<BooksPage />} />
-          <Route path="/loans" element={<LoansPage />} />
-          <Route path="/admin" element={<AdminPage />} />
+          <Route path="/books" element={
+            <ProtectedRoute>
+              <BooksPage />
+            </ProtectedRoute>
+          } />
+          <Route path="/loans" element={
+            <ProtectedRoute>
+              <LoansPage />
+            </ProtectedRoute>
+          } />
+          <Route path="/admin" element={
+            <ProtectedRoute adminOnly>
+              <AdminPage />
+            </ProtectedRoute>
+          } />
         </Routes>
         <Footer />
       </BrowserRouter>
